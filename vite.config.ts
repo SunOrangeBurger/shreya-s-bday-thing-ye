41f9e577c8e80f72027b60ee8b4a53b5c60a98f6
Arun <r.arunhariharan@gmail.com>
2026-02-17 23:02:06 +0530

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/"
+  base: process.env.VITE_BASE_PATH || "/shreya-s-bday-thing-ye"
   },
 );
